5158122ae3efc5918df884ea49ba503e0972d7d8
Palomastos <146142037+Palomastos@users.noreply.github.com>
2023-10-13 18:22:15 -0300

    Criação das cartas( front, back) e adição  botão

--- js/game.js
+++ js/game.js
@@ -1,26 +1,54 @@
 const grid = document.querySelector('.grid');
 const spanPlayer = document.querySelector('.player');
 const timer = document.querySelector('.timer');
-
+const dificulty = localStorage.getItem('dificulty')
 
 // A const 'accountant' serve para verificar a rodada no qual o player está.
 const accountant = localStorage.getItem('acc');
 // A const 'amountMov' serve para armazenar a quantidade de movimentos que o player fará nas cartas.
 const amountMov = [];
 
+// A função CheckArray cria três arrays, verifica-as e retorna a arrays dos respectivos níveis 
+// (Easy, medium e hard)
+const checkArray = () => {
+  const charactersEasy = [
+    'adicao',
+    'baskara',
+    'brioti',
+    'comparacao',
+    'priGrau',
+    'funcAfim',
+    'pitagora',
+    'substituicao',
+  ];
 
-const characters = [
-  'beth',
-  'jerry',
-  'jessica',
-  'morty',
-  'pessoa-passaro',
-  'pickle-rick',
-  'rick',
-  'summer',
-  'meeseeks',
-  'scroopy',
-];
+  const charactersMedium = [
+    'complQuadrado',
+    'crammer',
+    'escalonamento',
+    'idArcosDuplos',
+    'idPitagorica',
+    'leiCossenos',
+    'leiSeno',
+    'teoTales',
+  ];
+
+  const charactersHard = [
+  'algebra',
+  'edDiferenciais',
+  'euler',
+  'fermat',
+  'fourier',
+  'laplace',
+  'newton',
+  'valorMedio',
+  ];
+
+  if (dificulty== 'easy') {return charactersEasy}
+  else if (dificulty=='medium') {return charactersMedium}
+  else {return charactersHard}
+}
+const characters = checkArray()
 
 
 const createElement = (tag, className) => {
@@ -80,7 +108,6 @@ const checkCards = () => {
 
 }
 
-
 const revealCard = ({ target }) => {
   if (target.parentNode.className.includes('reveal-card')) {
     return;
@@ -98,47 +125,108 @@ const revealCard = ({ target }) => {
   }
 }
 
+// A const CreateCard cria as cartas do jogo que recebe como argumento elementos armazenados em character,
+//define suas faces (frontal e traseira) associada a uma imagem e cria um botão na parte da frente da carta.
+// também define um evento de clique e atribui um atributo personalizado à carta, retorna a carta configurada.
 
 const createCard = (character) => {
+  // const card cria um elemento de carta, onde a função createElement recebe  dois argumentos, uma div que é 
+  //um elemento em HTML usado para agrupar e organizar outros elementos e card que é a classe CSS 
+  //associada a esse elemento.
+    const card = createElement('div', 'card');
 
-  const card = createElement('div', 'card');
-  const front = createElement('div', 'face front');
-  const back = createElement('div', 'face back');
+    // Cria a parte frontal da carta (frente da carta)
+    const front = createElement('div', 'face front');
 
-  front.style.backgroundImage = `url('../images/${character}.png')`;
+     // Cria a parte traseira da carta (costas da carta)
+    const back = createElement('div', 'face back');
 
-  card.appendChild(front);
-  card.appendChild(back);
+    //const block = createElement('div', 'face block');
 
-  card.addEventListener('click', revealCard);
-  card.setAttribute('data-character', character)
+    // Cria um novo elemento HTML do tipo button.
+    const button = document.createElement('button');
 
-  return card;
-}
+    // A const button cria um botão com as classes 'face' e 'button' e o texto 'Dicas' dentro dele
+    button.classList.add('face', 'button');
+    button.textContent = 'Dicas'
 
+    //appendChild adiciona o elemento front (parte da frente) na card (carta) que está sendo criada
+    card.appendChild(front);
+     //appendChild adiciona o elemento button (botão) na card (carta) que está sendo criada
+    card.appendChild(button)
+    //appendChild adiciona o elemento back (parte traseira) na card (carta) que está sendo criada
+    card.appendChild(back);
+    //card.appendChild(block);
+  
+    // Atribuindo funcionalidades e atributos a carta criada 
+      // addEventListenner  É um método que permite que você associe um evento a um elemento HTML.
+      // Neste caso, ele está recebendo 2 argumentos, o evento (click) é o clique do mouse  
+      // e função revealCard que será chamada quando o evento de clique ocorrer. Portanto, esta linha 
+      // de código é para chamar a função revealCard sempre que a carta for clicada.
+    card.addEventListener('click', revealCard);
 
+    //o setAttribute  permite definir um atributo personalizado em um elemento HTML. Neste caso ele 
+    //recebe dois 2 argumentos, o data-character que é um atributo personalizado usado para armazenar
+    // informações adicionais sobre o elemento caracter que possui informação sobre o personagem associado a essa carta.
+    card.setAttribute('data-character', character);
+  
+    // O front.style.backgroundImage, define o caminho da imagem de fundo da face frontal da carta. De acordo
+    //com o personagem e o nível de dificuldades associada a carta.
+    front.style.backgroundImage = `url('../images/${dificulty}/${character}.jpg')`;
+
+    // Indica que a função createCard está retornando um valor que é o elemento card (carta) que foi criado
+    //  e configurado.
+    return card;
+  }
+
+  // comentar.....
 const loadGame = () => {
   const duplicateCharacters = [...characters, ...characters];
 
   const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
 
-  shuffledArray.forEach((character) => {
-    const card = createCard(character);
-    grid.appendChild(card);
-  });
+  const creaftingCard = ([characters, ...otherscharacter], acc=0)=> {
+    if(acc==shuffledArray.length) {
+      return;
+    }
+    else {
+      const card = createCard(characters);
+      grid.appendChild(card)
+      return creaftingCard(otherscharacter, acc+1);
+    }
+  }
+  creaftingCard(shuffledArray);
+
+  // shuffledArray.forEach((character) => {
+  //   const card = createCard(character);
+  //   grid.appendChild(card);
+  // });
 }
 
 
-// Função que cria um loop de tempo, incrementando +1 a cada 1 segundo, e, ao mesmo tempo, pelo o 'timer.innerHTML', adiciona e atualiza esse valor increntado na tela. Além disso, ela salva e atualiza esse tempo no localStorage.
+// Função que cria um loop de tempo, incrementando +1 a cada 1 segundo, e, ao mesmo tempo, pelo o 'timer.innerHTML', 
+// adiciona e atualiza esse valor increntado na tela. Além disso, ela salva e atualiza esse tempo no localStorage.
 const startTimer = () => {
+    if (stopTime.length == 1){
+      return;
+    }
+    else {
+      return setTimeout(()=> {
+        const currentTime = +timer.innerHTML;
+        timer.innerHTML = currentTime + 1;
+        localStorage.setItem(`time${accountant}`, currentTime);
+        startTimer ()
+      }, 1000)
+    }
+  }
+  // this.loop = setInterval(() => {
+  //   const currentTime = +timer.innerHTML;
+  //   timer.innerHTML = currentTime + 1;
 
-  this.loop = setInterval(() => {
-    const currentTime = +timer.innerHTML;
-    timer.innerHTML = currentTime + 1;
+  //   localStorage.setItem(`time${accountant}`, currentTime);
+  //   startTimer ()
+  // }, 1000);}
 
-    localStorage.setItem(`time${accountant}`, currentTime);
-  }, 1000);
-}
 
 
 // Comando que serve para executar as funções passadas nele somente quando a pág for totalmente carregada, a fim de evitar erros, já que alguns elementos das funções passadas necessitam que a parte html e as imagens carreguem primeiro. Além disso, pega o player digitado no login e adiciona na tela do jogo.
